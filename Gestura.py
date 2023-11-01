30ff2b821e07f485ccf6b9b09fb68862d77634c9
Ben <42848659+BenGravell@users.noreply.github.com>
2023-10-31 23:19:54 -0400

    Beautify welcome and README cleanup

--- Gestura.py
+++ Gestura.py
@@ -6,20 +6,37 @@ import streamlit as st
 import app_utils
 
 
-st.set_page_config(page_title="Gestura", page_icon="🤌", layout="wide")
+def streamlit_setup():
+    st.set_page_config(page_title="Gestura", page_icon="🤌", layout="wide")
 
-if not st.session_state.get("init"):
-    app_utils.first_time()
 
-st.title("Gestura")
-st.subheader("*An app for visualizing & introspecting predictions of gestures from time-series data.*", anchor=False)
-st.divider()
-st.subheader("Use Cases", anchor=False)
-st.markdown("""
-- Get a quantitative assessment of the model's predictive capabilities.
-- Introspect individual examples to understand *why* the model arrived at its predictions.
-- Learn about state-of-the-art deep learning model architectures.
-- Learn about open-source time-series classification datasets.
-""")
+def welcome():
+    st.title("🤌 Gestura")
+    st.caption("Visualize & introspect predictions of gestures from time-series data.")
+    
+    cols = st.columns([2, 1])
 
-st.success("Explore the other pages in the sidebar for more details.", icon="↩️")
+    with cols[0]:
+        st.subheader("What is Gestura?", divider="blue", anchor=False)
+
+        st.markdown("Gestura is an app that lets you:")
+        st.markdown("""
+        - Get a quantitative assessment of the model's predictive capabilities.
+        - Introspect individual examples to understand *why* the model arrived at its predictions.
+        - Learn about state-of-the-art deep learning model architectures.
+        - Learn about open-source time-series classification datasets.
+        """)
+
+        st.success("Explore the other pages in the sidebar for more details.", icon="↩️")
+
+    with cols[1]:
+        st.image("assets/welcome.jpg")
+
+
+if __name__ == "__main__":
+    streamlit_setup()    
+
+    if not st.session_state.get("init"):
+        app_utils.first_time()
+
+    welcome()
--- README.md
+++ README.md
@@ -1,19 +1,11 @@
 # Gestura
 Gesture prediction from time-series data.
 
-
 ## Usage
+[required]
 1. Run `train.py` to train the model.
 2. Run `streamlit run Gestura.py` to visualize the predictions.
 
-
-(optional)
-- Run `train_test_split.py` to regenerate the train/test split indices.
-- Run `cleanup.py` to remove old model and optimizer checkpoints.
-
-
-## Example Screenshots
-
-<div align="center">
-<img src="assets/gestura_example.png" alt="gestura_example"></img>
-</div>
+[optional]
+1. Run `train_test_split.py` to regenerate the train/test split indices.
+2. Run `cleanup.py` to remove old model and optimizer checkpoints.
